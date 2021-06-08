b4e5f7a668159e4a4288be6484e1e5fcfb0a46c4
Olegpup <71699911+Olegpup@users.noreply.github.com>
2021-06-08 09:33:53 +0300

    Add files via upload

---
+++ lab_7/lab_7_cpp/lab_7_cpp.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class WrongOperand : exception          //my own exception class
+{
+private:
+    string text;
+public:
+    WrongOperand(string text)
+    {
+        this->text = text;
+    }
+
+    string Text()
+    {
+        return text;
+    }
+};
+
+class Foo
+{
+private:     
+    double a;        //values of numbers
+    double b;
+    double c;
+    double d;
+
+public:
+    Foo() = default;
+
+    Foo(double a, double b, double c, double d)        //polinom function
+    {
+        if (a == 4 || a <= 0)
+        {
+            throw WrongOperand("Value of a is invalid");
+        }
+
+        this->a = a;
+        this->b = b;
+        this->c = c;
+        this->d = d;
+    }
+
+    void SetA(double value)              //set functions
+    {
+        if (value == 4 || value <= 0)
+        {
+            throw WrongOperand("Value of a is invalid");        //throwing exception
+        }
+
+        a = value;
+    }
+
+    void SetB(double value)
+    {
+        b = value;
+    }
+
+    void SetC(double value)
+    {
+        c = value;
+    }
+
+    void SetD(double value)
+    {
+        d = value;
+    }
+
+    double GetA()              //get functions
+    {
+        return a;
+    }
+
+    double GetB()
+    {
+        return b;
+    }
+
+    double GetC()
+    {
+        return c;
+    }
+
+    double GetD()
+    {
+        return d;
+    }
+
+    double GetValue()
+    {
+        return (2 * c - d + sqrt(23 * a)) / (a / 4 - 1);
+    }
+};
+
+int main()
+{
+    auto polynom1 = Foo();
+    try 
+    {
+        double a;
+        double b;
+        double c;
+        double d;
+
+        cout << "Enter a: ";
+        cin >> a;
+        polynom1.SetA(a);
+
+        cout << "Enter b: ";
+        cin >> b;
+        polynom1.SetB(b);
+
+        cout << "Enter c: ";
+        cin >> c;
+        polynom1.SetC(c);
+
+        cout << "Enter d: ";
+        cin >> d;
+        polynom1.SetD(d);
+
+        auto polynom2 = Foo(12, 1, 3, 7);
+
+        cout << "a, b, c, d" << endl;
+        cout << polynom1.GetA() << " ";
+        cout << polynom1.GetB() << " ";
+        cout << polynom1.GetC() << " ";
+        cout << polynom1.GetD() << " ";
+        cout << polynom1.GetD() << " = " << polynom1.GetValue() << endl << endl;
+        cout << "a, b, c, d" << endl;
+        cout << polynom2.GetA() << " ";
+        cout << polynom2.GetB() << " ";
+        cout << polynom2.GetC() << " ";
+        cout << polynom2.GetD() << " ";
+        cout << polynom2.GetD() << " = " << polynom2.GetValue() << endl << endl;
+    }
+    catch (WrongOperand& exc)
+    {
+        cout << exc.Text();
+    }
+}
---
+++ lab_7/lab_7_cs/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace lab_7_cs
+{
+    class WrongOperand : Exception          //my own exception class 
+    {
+        public string Text { private set; get; }
+
+        public WrongOperand(string a)
+        {
+            Text = a;
+        }
+    }
+
+    class Foo
+    {
+        private double _a;
+
+        public double A         //propertys
+        {
+            get
+            {
+                return _a;  
+            }
+            set
+            {
+                if (value == 4 || value < 0)
+                {
+                    throw new WrongOperand("Value of a is invalid");        //throwing exception 
+                }
+                _a = value;
+            }
+        }
+        public double B { get; set; }
+        public double C { get; set; }
+        public double D { get; set; }
+
+        public Foo()
+        {
+
+        }
+
+        public Foo(double a, double b, double c, double d)        //polinom function
+        {
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+        }
+
+        public double GetValue()
+        {
+            return (2 * C - D + Math.Sqrt(23 * A)) / (A / 4 - 1);
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            var polynom1 = new Foo();
+
+            try
+            {
+                Console.WriteLine("Enter a: ");
+                polynom1.A = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter b: ");
+                polynom1.B = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter c: ");
+                polynom1.C = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter d: ");
+                polynom1.D = Convert.ToDouble(Console.ReadLine());
+
+                var polynom2 = new Foo(12, 1, 3, 7);
+                
+                Console.WriteLine($"a, b, c, d \nF({polynom1.A}, {polynom1.B}, {polynom1.C}, {polynom1.D}) = {polynom1.GetValue()}");
+                Console.WriteLine($"a, b, c, d \nF({polynom2.A}, {polynom2.B}, {polynom2.C}, {polynom2.D}) = {polynom2.GetValue()}");
+            }
+            catch (WrongOperand exc)
+            {
+                Console.WriteLine(exc.Text);
+            }
+            Console.ReadKey();
+        }
+    }
+}
